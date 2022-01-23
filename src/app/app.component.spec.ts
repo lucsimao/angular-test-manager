85d97b19ea0b61ed116512fdfa907c602836b7f2
lucsimao <lucasimao64@gmail.com>
2022-01-23 20:27:12 -0300

    chore: update app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,31 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('Should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-manager'`, () => {
+  it(`Should have as title 'test-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('test-manager');
   });
 
-  it('should render title', () => {
+  it('Should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'test-manager app is running!'
+    );
   });
 });
