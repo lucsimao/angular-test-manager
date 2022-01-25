5e0bc4134561b076f80e70d681684e4aa8bb1bab
lucsimao <lucasimao64@gmail.com>
2022-01-24 21:34:11 -0300

    feat: ensure display TestCase when page is loaded

--- src/app/test-case/test-case.component.spec.ts
+++ src/app/test-case/test-case.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { TestCase } from './models/test-case';
 import { TestCaseComponent } from './test-case.component';
 
 const makeSut = async () => {
@@ -10,10 +11,27 @@ const makeSut = async () => {
   return { sut, fixture };
 };
 
+const makeFakeTestCase = (): TestCase => ({ name: 'Any Test Case' });
+
 describe(TestCaseComponent.name, () => {
   it('Should create Fixture', async () => {
     const { sut } = await makeSut();
 
     expect(sut).toBeTruthy();
+  });
+
+  describe('Dom', () => {
+    it('Should display TestCase when page is loaded', async () => {
+      const { sut, fixture } = await makeSut();
+
+      const fakeTestCase = makeFakeTestCase();
+      sut.testCase = fakeTestCase;
+      fixture.detectChanges();
+
+      const element: HTMLElement =
+        fixture.nativeElement.querySelector('.test-case');
+
+      expect(element.textContent?.trim()).toEqual(fakeTestCase.name);
+    });
   });
 });
--- src/app/test-case/test-case.component.ts
+++ src/app/test-case/test-case.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { TestCase } from './models/test-case';
 
 @Component({
   selector: 'app-test-case',
   templateUrl: './test-case.component.html',
   styleUrls: ['./test-case.component.scss'],
 })
-export class TestCaseComponent {}
+export class TestCaseComponent {
+  @Input() public testCase!: TestCase;
+}
