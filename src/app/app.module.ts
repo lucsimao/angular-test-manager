559b1af56e3bbf823cb4f5d3c7526215ae0a89cc
lucsimao <lucasimao64@gmail.com>
2022-01-26 01:01:26 -0300

    refactor: update modules in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TestCaseModule } from './test-case/test-case.module';
+import { TestCaseStepModule } from './test-case-step/test-case-step.module';
 
 @NgModule({
-  declarations: [AppComponent, TestCaseModule],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, TestCaseModule, TestCaseStepModule],
   providers: [],
   bootstrap: [AppComponent],
 })
